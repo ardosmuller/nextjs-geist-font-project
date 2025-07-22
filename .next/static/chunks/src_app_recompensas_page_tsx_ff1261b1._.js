0bdc0d066b75a93a8d64e704381f048bd36c5869
ardosmuller <triadegrouph@gmail.com>
2025-07-22 15:43:57 -0300

    Add .next/static/chunks/src_app_recompensas_page_tsx_ff1261b1._.js 1753189282941

---
+++ .next/static/chunks/src_app_recompensas_page_tsx_ff1261b1._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_980bc8d8._.js",
+  "static/chunks/src_8c3e426f._.js"
+],
+    source: "dynamic"
+});
