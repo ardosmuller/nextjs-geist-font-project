85fdd6ef587c0542b41f5d5c964997cc8189bf74
ardosmuller <triadegrouph@gmail.com>
2025-07-22 15:43:46 -0300

    Add .next/static/chunks/src_app_perfil_page_tsx_ff1261b1._.js 1753189271990

---
+++ .next/static/chunks/src_app_perfil_page_tsx_ff1261b1._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_3a34f9db._.js",
+  "static/chunks/src_620b8e3f._.js"
+],
+    source: "dynamic"
+});
